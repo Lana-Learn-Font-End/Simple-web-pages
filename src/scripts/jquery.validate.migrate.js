9164f308dacd6551b621b07e3b7e5d3d7d55f108
Lana <sitdownrightnow2552@gmail.com>
2019-11-12 21:31:35 +0700

    implement jquery validate

---
+++ src/scripts/jquery.validate.migrate.js
@@ -0,0 +1,29 @@
+// jquery validate with bootstrap 4
+jQuery.validator.setDefaults({
+    errorElement: "div",
+    errorClass: "invalid-feedback",
+    errorPlacement: function (error, element) {
+        element.closest(".form-group").append(error);
+    },
+    //on error-handle
+    highlight: function (element, errorClass, validClass) {
+        $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).removeClass("is-invalid");
+    },
+    //on form-submit
+    submitHandler: function (form) {
+        alert("Submitted!");
+    }
+});
+
+
+$.validator.addMethod(
+    'regex',
+    (value, element, param) => {
+        return this.optional(element) ||
+            value.match(typeof param == 'string' ? new RegExp(param) : param);
+    },
+    'Please enter a value in the correct format.'
+);
